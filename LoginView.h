6411db550216da09ea653d76d817f0c7dbdba457
EFerrante2002 <169213135+EFerrante2002@users.noreply.github.com>
2025-02-24 16:48:04 -0800

    Update LoginView.h

--- LoginView.h
+++ LoginView.h
@@ -2,8 +2,33 @@
 #define LOGINVIEW_H
 
 #include <string>
+#include "UserProfile.h"
 
-void readUsernameTextbox();
+class LoginView{
+    public: 
+        void readUsernameTextbox();
+        void readPassWordTextbox();
+        void readEmailTextbox();
+        void readNotificationPreferences();
+
+        void registerUser(UserProfile profile, string password);
+        void transitionToHomeView(UserProfile profile);
+
+        void displayError(string &errorText);
+
+        void createNotificationPreferences();
+        void createUserProfile();
+
+    private:
+        UserProfile* profile;
+        string username;
+        string password;
+        string email;
+        NotificationPreferences preferences;
+
+
+};
+
 
 
 
